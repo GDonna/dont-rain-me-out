6e51a5e62e7d40af1bc4c769347e0fd2efc61474
Stephen H <Sthogue16@gmail.com>
2023-07-13 19:47:24 -0500

    added apis

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var APImapKey = "Ajsz-0Rnl_91Q4o_7Zk-1vDOqfxgvrUPg5-qABhbSNWKgUbRWqI19-Ll37r6mExo"
 var APIeventKey = "9m1sGkEcZegpwhG1afNONOAPhT8SAZVM"
 
+var fetchButton = document.querySelector("#click-button")
+
 // API to Ticketmaster - Get Event Details? 
 
 
 function getApi() {
  
-      var requestUrl = "https://app.ticketmaster.com/discovery/v2/events/.json?countryCode=US&apikey="+ APIeventKey;
+      var requestUrl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=9m1sGkEcZegpwhG1afNONOAPhT8SAZVM";
 
     fetch(requestUrl)
     .then(function (response) {
-      return response.json();
+      if (response.ok) {
+        console.log(response);
+      }
     });
 
 }
