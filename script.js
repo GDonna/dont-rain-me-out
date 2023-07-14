29a518061ec0064eca60adf2a47f3c921c2b6065
Donna Goon <g.donna120@gmail.com>
2023-07-13 19:18:28 -0500

    Added API Keys

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-
+var APImapKey = "Ajsz-0Rnl_91Q4o_7Zk-1vDOqfxgvrUPg5-qABhbSNWKgUbRWqI19-Ll37r6mExo"
+var APIeventKey = "9m1sGkEcZegpwhG1afNONOAPhT8SAZVM"
 
 // API to Ticketmaster - Get Event Details? 
+
+
 function getApi() {
  
-    var requestUrl = "https://app.ticketmaster.com/discovery/v2/events/G5diZfkn0B-bh.json?apikey={apikey}&per_page=5"
+      var requestUrl = "https://app.ticketmaster.com/discovery/v2/events/.json?countryCode=US&apikey="+ APIeventKey;
 
     fetch(requestUrl)
     .then(function (response) {
