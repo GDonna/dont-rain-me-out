86a63b82f3382daabf3622218b3fe736094d5815
Stephen H <Sthogue16@gmail.com>
2023-07-24 13:15:31 -0500

    fixed image header

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,68 +1,48 @@
-var searchEventEl = document.querySelector('.card');
-var errorEl = document.getElementById('error');
-var errorEl2 = document.getElementById('error2')
+var searchEventEl= document.querySelector('.card')
 
-// Function for first modal error message
-function error() {
+var modal = document.getElementById("myModal")
+var span = document.getElementsByClassName("close")[0];
 
-  if (errorEl.classList.contains('hidden')) {
-    errorEl.classList.replace('hidden', 'display')
-  } else {
-    errorEl.classList.replace('display', 'hidden')
-  }
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-}
-
-//Function for second modal error message
-function error2() {
-
-  if (errorEl2.classList.contains('hidden')) {
-    errorEl2.classList.replace('hidden', 'display')
-  } else {
-    errorEl2.classList.replace('display', 'hidden')
-  }
-
-}
-
-
-
-function searchCityStateSubmit(event) {
+function searchCityStateSubmit (event) {
   event.preventDefault();
-
+  
   var cityFormEl = document.querySelector('#cityDropDown').value;
   var stateFormEl = document.querySelector('#stateDropDown').value;
 
   if (!cityFormEl || !stateFormEl) {
-    error()
+    modal.style.display = "show"
+    // console.error ("Please enter a city and state");
     return;
-  }
-  function checkLocation() {
-    var apiUrl = "https://app.ticketmaster.com/discovery/v2/events?apikey=9m1sGkEcZegpwhG1afNONOAPhT8SAZVM&radius=10&unit=miles&locale=*&sort=date,asc&city=" + cityFormEl + "&countryCode=US&stateCode=" + stateFormEl;
+  } 
+  function checkLocation () {
+    var apiUrl = "https://app.ticketmaster.com/discovery/v2/events?apikey=9m1sGkEcZegpwhG1afNONOAPhT8SAZVM&radius=10&unit=miles&locale=*&sort=date,asc&city="+cityFormEl+"&countryCode=US&stateCode="+ stateFormEl;
 
     console.log(apiUrl)
-
+    
     fetch(apiUrl)
-      .then(function (response) {
+    .then (function (response) {
         console.log("RES: ", response)
-        return response.json()
-      })
-      .then(function (data) {
-        console.log(data)
-        if (data.page.totalElements === 0) {
+         return response.json()
+    })
+    .then (function (data){
           console.log(data)
-          error2();
-          return null;
-        }
-        if (data.page.totalElements !== 0) {
-          var searchEvent = "./eventpage.html?q=" + cityFormEl + "&format=" + stateFormEl;
-          location.assign(searchEvent)
-        }
-
-      })
-      .catch(err => console.log(err))
-  }
-  checkLocation()
-}
-searchEventEl.addEventListener("submit", searchCityStateSubmit)
-errorEl.addEventListener("click", error)
-errorEl2.addEventListener("click", error2)
+          if (data.page.totalElements === 0){
+            console.log(data)
+            alert('Error: ')
+            return null;
+          }
+          if (data.page.totalElements !== 0){
+            var searchEvent = "./eventpage.html?q=" +cityFormEl+ "&format=" +stateFormEl;
+            location.assign(searchEvent)  
+          }
+        
+    })
+    .catch(err => console.log(err)) 
+      }
+      checkLocation()
+}  
+    searchEventEl.addEventListener("submit", searchCityStateSubmit)
